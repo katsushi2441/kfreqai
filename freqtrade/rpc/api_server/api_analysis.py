8aee23eef869857a006aa95f186c0504434fa3aa
Matthias <xmatthias@outlook.com>
2026-06-27 17:16:41 +0200

    refactor: combine analysis function run

--- freqtrade/rpc/api_server/api_analysis.py
+++ freqtrade/rpc/api_server/api_analysis.py
@@ -1,6 +1,8 @@
 import asyncio
 import logging
+from collections.abc import Callable
 from copy import deepcopy
+from typing import Any, Literal
 
 from fastapi import APIRouter, BackgroundTasks, Depends
 from fastapi.exceptions import HTTPException
@@ -27,10 +29,39 @@ router_recursive = APIRouter()
 router_lookahead = APIRouter()
 
 
-def __run_recursive_analysis_bg(config_loc: Config, job_id: str):
-    from freqtrade.optimize.analysis.recursive_helpers import RecursiveAnalysisSubFunctions
+def __resolve_strategy_object(config_loc: Config) -> dict:
+    """Resolve the strategy object the way analysis functions expect it"""
     from freqtrade.resolvers.strategy_resolver import StrategyResolver
 
+    strategy_obj = next(
+        (
+            s
+            for s in StrategyResolver.search_all_objects(
+                config_loc,
+                enum_failed=False,
+                recursive=config_loc.get("recursive_strategy_search", False),
+            )
+            if s["name"] == config_loc["strategy"]
+        ),
+        None,
+    )
+    if not strategy_obj:
+        raise ConfigurationError(f"Strategy {config_loc['strategy']} not found.")
+    return strategy_obj
+
+
+def __run_analysis_bg(
+    config_loc: Config,
+    job_id: str,
+    analysis_name: Literal["Recursive", "Lookahead"],
+    run_analysis: Callable[[Config, Any], dict],
+):
+    """
+    :param config_loc: The configuration to use for the analysis
+    :param job_id: The job ID for the analysis
+    :param analysis_name: The name of the analysis |
+    :param run_analysis: The function to run the analysis.
+    """
     job = ApiBG.jobs[job_id]
     job["is_running"] = True
     asyncio.set_event_loop(asyncio.new_event_loop())
@@ -44,27 +75,31 @@ def __run_recursive_analysis_bg(config_loc: Config, job_id: str):
             }
 
         pt = get_progress_tracker(ft_callback=ft_callback)
-        config_loc = RecursiveAnalysisSubFunctions.calculate_config_overrides(config_loc)
-        strategy_obj = next(
-            (
-                s
-                for s in StrategyResolver.search_all_objects(
-                    config_loc,
-                    enum_failed=False,
-                    recursive=config_loc.get("recursive_strategy_search", False),
-                )
-                if s["name"] == config_loc["strategy"]
-            ),
-            None,
-        )
-        if not strategy_obj:
-            raise ConfigurationError(f"Strategy {config_loc['strategy']} not found.")
+        job["result"] = run_analysis(config_loc, pt)
+        job["status"] = "success"
+    except ConfigurationError as e:
+        logger.error(f"{analysis_name} analysis encountered a configuration Error: {e}")
+        job["status"] = "failed"
+        job["error"] = str(e)
+    except (Exception, OperationalException, DependencyException) as e:
+        logger.exception(f"{analysis_name} analysis caused an error: {e}")
+        job["status"] = "failed"
+        job["error"] = str(e)
+    finally:
+        job["is_running"] = False
+        ApiBG.analysis_running = False
 
+
+def __run_recursive_analysis_bg(config_loc: Config, job_id: str):
+    from freqtrade.optimize.analysis.recursive_helpers import RecursiveAnalysisSubFunctions
+
+    def run_analysis(config_loc: Config, pt: Any) -> dict:
+        config_loc = RecursiveAnalysisSubFunctions.calculate_config_overrides(config_loc)
+        strategy_obj = __resolve_strategy_object(config_loc)
         instance = RecursiveAnalysisSubFunctions.initialize_single_recursive_analysis(
             config_loc, strategy_obj, pt
         )
-
-        job["result"] = {
+        return {
             "strategy": instance.strategy_obj["name"],
             "startup_candles": instance._startup_candle,
             "strategy_scc": instance._strat_scc,
@@ -73,18 +108,8 @@ def __run_recursive_analysis_bg(config_loc: Config, job_id: str):
                 for indicator, values in instance.dict_recursive.items()
             },
         }
-        job["status"] = "success"
-    except ConfigurationError as e:
-        logger.error(f"Recursive analysis encountered a configuration Error: {e}")
-        job["status"] = "failed"
-        job["error"] = str(e)
-    except (Exception, OperationalException, DependencyException) as e:
-        logger.exception(f"Recursive analysis caused an error: {e}")
-        job["status"] = "failed"
-        job["error"] = str(e)
-    finally:
-        job["is_running"] = False
-        ApiBG.analysis_running = False
+
+    __run_analysis_bg(config_loc, job_id, "Recursive", run_analysis)
 
 
 @router_recursive.post("/recursive_analysis", response_model=BgJobStarted)
@@ -154,42 +179,15 @@ def api_get_lookahead_analysis(jobid: str):
 
 def __run_lookahead_analysis_bg(config_loc: Config, job_id: str):
     from freqtrade.optimize.analysis.lookahead_helpers import LookaheadAnalysisSubFunctions
-    from freqtrade.resolvers.strategy_resolver import StrategyResolver
 
-    job = ApiBG.jobs[job_id]
-    job["is_running"] = True
-    asyncio.set_event_loop(asyncio.new_event_loop())
-    try:
-
-        def ft_callback(task) -> None:
-            job["progress_tasks"][str(task.id)] = {
-                "progress": task.completed,
-                "total": task.total,
-                "description": task.description,
-            }
-
-        pt = get_progress_tracker(ft_callback=ft_callback)
+    def run_analysis(config_loc: Config, pt: Any) -> dict:
         config_loc = LookaheadAnalysisSubFunctions.calculate_config_overrides(config_loc)
-        strategy_obj = next(
-            (
-                s
-                for s in StrategyResolver.search_all_objects(
-                    config_loc,
-                    enum_failed=False,
-                    recursive=config_loc.get("recursive_strategy_search", False),
-                )
-                if s["name"] == config_loc["strategy"]
-            ),
-            None,
-        )
-        if not strategy_obj:
-            raise ConfigurationError(f"Strategy {config_loc['strategy']} not found.")
-
+        strategy_obj = __resolve_strategy_object(config_loc)
         instance = LookaheadAnalysisSubFunctions.initialize_single_lookahead_analysis(
             config_loc, strategy_obj, pt
         )
         analysis = instance.current_analysis
-        job["result"] = {
+        return {
             "strategy": instance.strategy_obj["name"],
             "has_bias": analysis.has_bias,
             "total_signals": int(analysis.total_signals),
@@ -197,18 +195,8 @@ def __run_lookahead_analysis_bg(config_loc: Config, job_id: str):
             "biased_exit_signals": int(analysis.false_exit_signals),
             "biased_indicators": analysis.false_indicators,
         }
-        job["status"] = "success"
-    except ConfigurationError as e:
-        logger.error(f"Lookahead analysis encountered a configuration Error: {e}")
-        job["status"] = "failed"
-        job["error"] = str(e)
-    except (Exception, OperationalException, DependencyException) as e:
-        logger.exception(f"Lookahead analysis caused an error: {e}")
-        job["status"] = "failed"
-        job["error"] = str(e)
-    finally:
-        job["is_running"] = False
-        ApiBG.analysis_running = False
+
+    __run_analysis_bg(config_loc, job_id, "Lookahead", run_analysis)
 
 
 @router_lookahead.post("/lookahead_analysis", response_model=BgJobStarted)
